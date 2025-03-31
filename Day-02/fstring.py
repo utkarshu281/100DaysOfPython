95ec3e8d8e2781da54a59296ae643842f7f6a4cc
utkarsh upadhyay <106943429+utkarshu281@users.noreply.github.com>
2025-03-31 13:39:59 +0530

    Create fstring.py

---
+++ Day-02/fstring.py
@@ -0,0 +1,10 @@
+# bmi = 84 / 1.65 ** 2
+# print(bmi)
+# print(int(bmi))
+# print(round(bmi))
+# print(round(bmi,2))
+#fstring
+score = 100
+height = 6
+are_u_winning = True
+print(f"Your score is = {score},Your height is{height},are you winning? {are_u_winning} ")
