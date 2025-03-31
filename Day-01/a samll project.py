6de7dad42080d5a15bc549562c1ab4f06c1a7dd7
utkarsh upadhyay <106943429+utkarshu281@users.noreply.github.com>
2025-03-31 07:26:33 +0530

    Create a samll project.py

---
+++ Day-01/a samll project.py
@@ -0,0 +1,4 @@
+print("Howdy!welcome to band name generator")
+city = input("enter the name of city u grew in:\n")
+pet = input("enter the name of your pet:")
+print("your band name could be "+city+""+pet+"\nhope u like the name please come back again")
