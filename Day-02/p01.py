15a7fa575e552ae1af5c38bb0de563fde79edc5d
utkarsh upadhyay <106943429+utkarshu281@users.noreply.github.com>
2025-03-31 07:35:42 +0530

    Create p01.py

---
+++ Day-02/p01.py
@@ -0,0 +1,9 @@
+len("Hello")
+a = " john"
+print(123456)#integer
+print("123" + "345")#wont add up to a sum its a string 
+print(123 + 456)#it will
+print(1234_456_789)#a way to write big number
+print(3.14)
+print(True)
+Print(False)
