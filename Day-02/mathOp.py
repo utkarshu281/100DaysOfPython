803b025e7c8400f97423b9deeb10ad45704fb0b8
utkarsh upadhyay <106943429+utkarshu281@users.noreply.github.com>
2025-03-31 08:17:38 +0530

    Create mathOp.py

---
+++ Day-02/mathOp.py
@@ -0,0 +1,18 @@
+print("My age: " + str(12))
+print(123+456)
+print(123-1)
+print(2*2)
+print(2**3)
+print(5/3)
+print(5//3)
+
+#PEMDAS(L TO R)
+#()
+#**
+# * or /
+# + or -
+print(3 * 3 + 3 / 3 - 3)
+# outout change to 9
+print(3*3/3+3-3)
+#angela mam sol
+print(3 * (3 + 3) / 3 - 3)
