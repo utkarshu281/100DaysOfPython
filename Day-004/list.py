8df308db33ee53a1305e07cec9059e24b6200f87
utkarsh upadhyay <106943429+utkarshu281@users.noreply.github.com>
2025-04-06 00:09:25 +0530

    Create list.py

---
+++ Day-004/list.py
@@ -0,0 +1,9 @@
+fruits = ["Cherry", "Apple", "Pear"]
+print(fruits[0])
+print(fruits[-1])
+fruits[0] = "orange"
+print(fruits)
+fruits.append("banana")
+print(fruits)
+fruits.extend(["mango","guava","kiwi"])
+print(fruits)
